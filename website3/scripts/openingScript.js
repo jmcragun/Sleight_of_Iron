470696ba414dd885acce2dac27c42754d8755764
jmcragun <jmcragun@gmail.com>
2016-07-13 11:34:03 -0600

    updated openingScript.js

--- website3/scripts/openingScript.js
+++ website3/scripts/openingScript.js
@@ -1,3 +1,10 @@
+var nextCounter = 0;
 $(document).ready(function () {
 	$("#whatsYourName").hide();
+	$("#next").click(function () {
+		if (nextCounter == 0) {
+			$("#whatsYourName").fadeIn(1000);
+			nextCounter++;
+		}
+	});
 });
