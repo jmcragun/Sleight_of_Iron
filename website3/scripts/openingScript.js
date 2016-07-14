5ded3056307bacba38a69778a343444f8fb2b358
jmcragun <jmcragun@gmail.com>
2016-07-14 09:44:40 -0600

    Fixed the cool stuff

--- website3/scripts/openingScript.js
+++ website3/scripts/openingScript.js
@@ -1,5 +1,6 @@
 var nextCounter = 0;
 var userName = "";
+var nameAccepted = false;
 $(document).ready(function () {
 	$("#whatsYourName").hide();
 	$("#next").click(function () {
@@ -12,7 +13,13 @@ $(document).ready(function () {
 			if (userName.length == 0) {
 				alert("Come on now, ye do have a name, don't ye?");
 			} else {
-				confirm("So, yer name is " + userName + "?");
+				var nameAccepted = confirm("So, yer name is " + userName + "?");
+				if (nameAccepted == true) {
+					$("#greeting").fadeOut(1000);
+					$("#whatsYourName").fadeOut(1000);
+					$("<p>Well then " + userName + ", a world in turmoil rests at your feet. Will you save it, or shall you destroy it?</p>").hide().appendTo("body").fadeIn(1000);
+					nextCounter++;
+				}
 			}
 		}
 	});
