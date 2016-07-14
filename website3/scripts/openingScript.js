dbde30a8d014d11ab0c5a6f3d7372cbcd0ab914a
jmcragun <jmcragun@gmail.com>
2016-07-14 11:14:51 -0600

    finalized openingpage (for now)

--- website3/scripts/openingScript.js
+++ website3/scripts/openingScript.js
@@ -7,8 +7,7 @@ $(document).ready(function () {
 		if (nextCounter == 0) {
 			$("#whatsYourName").fadeIn(1000);
 			nextCounter++;
-		}
-		else if (nextCounter == 1) {
+		} else if (nextCounter == 1) {
 			userName = $("#whatsYourName").val();
 			if (userName.length == 0) {
 				alert("Come on now, ye do have a name, don't ye?");
@@ -17,10 +16,18 @@ $(document).ready(function () {
 				if (nameAccepted == true) {
 					$("#greeting").fadeOut(1000);
 					$("#whatsYourName").fadeOut(1000);
-					$("<p>Well then " + userName + ", a world in turmoil rests at your feet. Will you save it, or shall you destroy it?</p>").hide().delay(1000).appendTo("body").fadeIn(1000);
+					var statement1 = "<p>Well then " + userName + ", a world in turmoil rests at your feet. Will you save it, or shall you destroy it?</p>";
+					$(statement1).hide().delay(1000).appendTo("body").fadeIn(1000);
 					nextCounter++;
 				}
 			}
+		} else if (nextCounter == 2) {
+			$(statement1).fadeOut(1000);
+			var statement2 = "<p>But before you are cast into this world, tell me about yourself.</p>";
+			$(statement2).hide().appendTo("body").fadeIn(1000);
+			nextCounter++;
+		} else if (nextCounter == 3) {
+			location.href='./storypages/classselector.html'
 		}
 	});
 });
