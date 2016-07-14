7f380a5933c69fac231ce907e7efc107c9842a23
jmcragun <jmcragun@gmail.com>
2016-07-14 08:56:22 -0600

    Updated script and css

--- website3/scripts/openingScript.js
+++ website3/scripts/openingScript.js
@@ -1,10 +1,19 @@
 var nextCounter = 0;
+var userName = "";
 $(document).ready(function () {
 	$("#whatsYourName").hide();
 	$("#next").click(function () {
 		if (nextCounter == 0) {
 			$("#whatsYourName").fadeIn(1000);
 			nextCounter++;
+		}
+		else if (nextCounter == 1) {
+			userName = $("#whatsYourName").val();
+			if (userName.length == 0) {
+				alert("Come on now, ye do have a name, don't ye?");
+			} else {
+				confirm("So, yer name is " + userName + "?");
+			}
 		}
 	});
 });
